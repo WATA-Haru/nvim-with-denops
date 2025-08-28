3add7f15862080ec9b165b3526d87bb34fbe6744
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-08-29 00:37:02 +0900

    feat: scroll速さ調整

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -25,3 +25,21 @@ require('neoscroll').setup({
       'WinScrolled', 'CursorMoved'
   },
 })
+
+-- neoscroll keys in lua/config/keys.lua
+neoscroll = require('neoscroll')
+local keymap = {
+  ["<C-u>"] = function() neoscroll.ctrl_u({ duration = 250 }) end;
+  ["<C-d>"] = function() neoscroll.ctrl_d({ duration = 250 }) end;
+  ["<C-b>"] = function() neoscroll.ctrl_b({ duration = 450 }) end;
+  ["<C-f>"] = function() neoscroll.ctrl_f({ duration = 450 }) end;
+  ["<C-y>"] = function() neoscroll.scroll(-0.1, { move_cursor=false; duration = 150 }) end;
+  ["<C-e>"] = function() neoscroll.scroll(0.1, { move_cursor=false; duration = 150 }) end;
+  ["zt"]    = function() neoscroll.zt({ half_win_duration = 250 }) end;
+  ["zz"]    = function() neoscroll.zz({ half_win_duration = 250 }) end;
+  ["zb"]    = function() neoscroll.zb({ half_win_duration = 250 }) end;
+}
+local modes = { 'n', 'v', 'x' }
+for key, func in pairs(keymap) do
+  vim.keymap.set(modes, key, func)
+end
