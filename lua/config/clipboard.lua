c8378cd91fd091a5409ca81e24af1e7572ad5cac
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-08-28 23:28:37 +0900

    fix: win32yankを絶対パスに変更

--- lua/config/clipboard.lua
+++ lua/config/clipboard.lua
@@ -1,14 +1,17 @@
+local home_dir = os.getenv("HOME")
+local win32yank_exe_path = home_dir .. "/.local/bin/win32yank.exe"
+
 -- clipboard
 if vim.fn.has('wsl') == 1 then
     vim.g.clipboard = {
         name = 'myClipboard',
         copy = {
-            ['+'] = 'win32yank.exe -i',
-            ['*'] = 'win32yank.exe -i',
+            ['+'] = win32yank_exe_path .. ' -i',
+            ['*'] = win32yank_exe_path .. ' -i',
         },
         paste = {
-            ['+'] = 'win32yank.exe -o',
-            ['*'] = 'win32yank.exe -o',
+            ['+'] = win32yank_exe_path .. ' -o',
+            ['*'] = win32yank_exe_path .. ' -o',
         },
         cache_enabled = 1,
     }
