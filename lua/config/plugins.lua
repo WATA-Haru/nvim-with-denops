ba9ca646c4af865839d8b0ded99ba7bfd8cff976
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-08-31 15:41:26 +0900

    feat: diffview導入

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -3,7 +3,8 @@ require('plugins.tender') -- colorscheme
 require('plugins.iceberg') -- colorscheme
 require('plugins.lackluster') -- minimal colorscheme
 require('plugins.treesitter') -- syntax highlighting
-require('plugins.oil') -- filer
+require('plugins.oil') -- main file viewer to move and rename file
+require('plugins.fern') -- sub file viewer to see directory structure
 require('plugins.nvim-lspconfig') -- lsp settings
 require('plugins.telescope') -- fuzzy finder
 require('plugins.gin') -- git plugin
@@ -11,3 +12,5 @@ require('plugins.zen-mode') -- focus mode
 require('plugins.snacks') -- useful tools (to rename file automatically when Oil changes file path)
 require('plugins.ddc') -- code completation
 require('plugins.neoscroll') -- scroll plugin
+require('plugins.diffview') -- diffview
+
