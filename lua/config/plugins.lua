85f49cf75416ebee6939956b369c05c5aec7fc06
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-09-09 18:21:22 +0900

    feat: add gitsigns

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -7,7 +7,8 @@ require('plugins.oil') -- main file viewer to move and rename file
 require('plugins.fern') -- sub file viewer to see directory structure
 require('plugins.nvim-lspconfig') -- lsp settings
 require('plugins.telescope') -- fuzzy finder
-require('plugins.gin') -- git plugin
+require('plugins.gin') -- git command plugin
+require('plugins.gitsigns') -- git blame view
 require('plugins.zen-mode') -- focus mode
 require('plugins.snacks') -- useful tools (to rename file automatically when Oil changes file path)
 require('plugins.ddc') -- code completation
