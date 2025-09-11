88a993fbdbe879eab6351d25c6deeeb102b932e1
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-09-11 15:49:12 +0900

    feat: add no-neck-pain

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -10,7 +10,8 @@ require('plugins.nvim-lspconfig') -- lsp settings
 require('plugins.telescope') -- fuzzy finder
 require('plugins.gin') -- git command plugin
 require('plugins.gitsigns') -- git blame view
-require('plugins.zen-mode') -- focus mode
+-- require('plugins.zen-mode') -- focus mode
+require('plugins.no-neck-pain') -- focus mode
 require('plugins.snacks') -- useful tools (to rename file automatically when Oil changes file path)
 require('plugins.ddc') -- code completation
 require('plugins.neoscroll') -- scroll plugin
---
+++ lua/plugins/no-neck-pain.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+  {
+    src = "https://github.com/shortcuts/no-neck-pain.nvim",
+    version = "main"
+  }
+})
+
