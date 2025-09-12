9a771d07ce658dcf9a64c1f7729cb10a318aced0
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-09-12 14:16:46 +0900

    feat: INFO, FIXME, TODOなどのコードハイライトを追加

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -4,13 +4,43 @@ local reading_colorscheme = "lackluster"
 local current_colorscheme = default_colorscheme
 
 local function override_colorscheme()
-  -- visual highlight override
+  -- visual mode highlight override (yellow color)
   vim.cmd[[highlight Visual  ctermfg=158 ctermbg=29 guifg=#c0c5b9 guibg=#45493e ]]
+end
+
+-- コメント中の特定の単語を強調表示する関数
+local function setup_todo_highlights()
+  local augroup = vim.api.nvim_create_augroup("HilightsForce", { clear = true })
+  
+  -- 各キーワード用のハイライトグループを定義
+  vim.cmd[[highlight TodoKeyword guibg=#FF6B6B guifg=White]]  -- 赤系 (TODO用)
+  vim.cmd[[highlight InfoKeyword guibg=#40BA8D guifg=White]]  -- 緑系 (INFO用)
+  vim.cmd[[highlight NoteKeyword guibg=NONE guifg=#45B7D1]]  -- 青系 (NOTE用)
+  vim.cmd[[highlight TempKeyword guibg=#FFA726 guifg=White]]  -- オレンジ系 (TEMP用)
+  vim.cmd[[highlight FIXMEKeyword guibg=NONE guifg=#FFC800]]   -- 黄色系(FIXME用)
+  
+  vim.api.nvim_create_autocmd(
+    { "WinEnter", "BufRead", "BufNew", "Syntax" },
+    {
+      group = augroup,
+      callback = function()
+        -- 各キーワードを個別にハイライト
+        pcall(vim.fn.matchadd, 'TodoKeyword', 'TODO')
+        pcall(vim.fn.matchadd, 'InfoKeyword', 'INFO')
+        pcall(vim.fn.matchadd, 'NoteKeyword', 'NOTE')
+        pcall(vim.fn.matchadd, 'TempKeyword', 'TEMP')
+        pcall(vim.fn.matchadd, 'FIXMEKeyword', 'FIXME')
+      end,
+    }
+  )
 end
 
 -- default
 pcall(vim.cmd, "colorscheme " .. default_colorscheme)
 override_colorscheme()
+
+-- TODO/NOTE/INFO ハイライトを設定
+setup_todo_highlights()
 
 -- Toggle between default and reading colorschemes
 vim.api.nvim_create_user_command(
@@ -34,6 +64,8 @@ vim.api.nvim_create_user_command(
       end
     end
     override_colorscheme()
+    -- カラースキーム変更後もTODOハイライトを再適用
+    setup_todo_highlights()
   end,
   { desc = "Toggle between " .. default_colorscheme .. " and " .. reading_colorscheme .. " colorschemes" }
 )
