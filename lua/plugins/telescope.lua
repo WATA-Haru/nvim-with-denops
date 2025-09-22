95caf8c708bee7efdd62809e0be217294444f311
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-09-22 11:39:00 +0900

    feat: telescope-insert-path.nvim を入れてファイルのパスをコピーできるようにした

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,20 @@
 -- telescope.nvim depends on plenary.nvim, so plenary must be loaded first
-vim.pack.add({
-  {
+vim.pack.add({ {
     src = 'https://github.com/nvim-lua/plenary.nvim',
     version = 'master'
   },
   {
     src = 'https://github.com/nvim-telescope/telescope.nvim',
     version = 'master'
+  },
+  {
+    src = 'https://github.com/kiyoon/telescope-insert-path.nvim',
+    version = 'master'
   }
 })
+
+-- insert path actions
+local path_actions = require('telescope_insert_path')
 
 require('telescope').setup({
   defaults = {
@@ -25,6 +31,24 @@ require('telescope').setup({
     file_ignore_patterns = {
       "^.git/",
       "^node_modules/",
+    },
+    mappings = {
+      n = {
+        -- insert path mappings
+        -- E.g. Type `[i`, `[I`, `[a`, `[A`, `[o`, `[O` to insert relative path and select the path in visual mode.
+        -- Other mappings work the same way with a different prefix.
+        ["["] = path_actions.insert_reltobufpath_visual,
+        ["]"] = path_actions.insert_abspath_visual,
+        ["{"] = path_actions.insert_reltobufpath_insert,
+        ["}"] = path_actions.insert_abspath_insert,
+        ["-"] = path_actions.insert_reltobufpath_normal,
+        ["="] = path_actions.insert_abspath_normal,
+	-- If you want to get relative path that is relative to the cwd, use
+	-- `relpath` instead of `reltobufpath`
+        -- You can skip the location postfix if you specify that in the function name.
+        -- ["<C-o>"] = path_actions.insert_relpath_o_visual,
+      }
     }
   }
 })
+
