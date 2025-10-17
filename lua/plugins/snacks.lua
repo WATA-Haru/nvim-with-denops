a24c9db1758b329ac0c41466f2a0f4e2088badd9
WATA-Haru <43723360+WATA-Haru@users.noreply.github.com>
2025-10-17 16:49:55 +0900

    fix: 使用していないsnacksのpropertyをenable falseに
    https://github.com/folke/snacks.nvim/blob/454ba02d69347c0735044f159b95d2495fc79a73/docs/init.md

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,20 +6,42 @@ vim.pack.add({
 })
 
 require("snacks").setup({
-  bigfile = { enabled = true },
-  dashboard = { enabled = false },
-  explorer = { enabled = false },
-  indent = { enabled = true },
-  input = { enabled = true },
-  picker = { enabled = true },
-  notifier = { enabled = false },
-  quickfile = { enabled = true },
-  scope = { enabled = true },
-  scroll = { enabled = false },
-  statuscolumn = { enabled = false },
+  rename = { enable = true },
+  image = { enable = true },
+  bigfile = { enable = true },
+  indent = { enable = true },
+  animate = { enable = false },
+  bufdelete = { enable = false },
+  dashboard = { enable = false },
+  debug = { enable = false },
+  dim = { enable = false },
+  explorer = { enable = false },
+  git = { enable = false },
+  gitbrowse = { enable = false },
+  health = { enable = false },
+  input = { enable = false },
+  layout = { enable = false },
+  lazygit = { enable = false },
+  meta = { enable = false },
+  notifier = { enable = false },
+  notify = { enable = false },
+  picker = { enable = false },
+  profiler = { enable = false },
+  quickfile = { enable = false },
+  scope = { enable = false },
+  scratch = { enable = false },
+  scroll = { enable = false },
+  statuscolumn = { enable = false },
+  terminal = { enable = false },
+  toggle = { enable = false },
+  util = { enable = false },
+  win = { enable = false },
+  words = { enable = false },
+  zen = { enable = false },
 })
 
 -- Auto path rename for Oil.nvim integration
+-- https://github.com/folke/snacks.nvim/blob/454ba02d69347c0735044f159b95d2495fc79a73/docs/rename.md?plain=1#L16
 vim.api.nvim_create_autocmd("User", {
   pattern = "OilActionsPost",
   callback = function(event)
